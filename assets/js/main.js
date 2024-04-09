b79278d9bf5953940fa1e0ab96b27143eb4c0bb4
unknown <gouravbishnoi429@gmail.com>
2024-04-09 15:01:45 +0530

    sec-2 added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,60 @@ function openNav() {
     document.body.classList.toggle("overflow_hidden");
     document.querySelector(".menu").classList.toggle("cross")
 }
+$('.responsive_1').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    speed: 900,
+    prevArrow: ".prev-btn",
+    nextArrow: ".next-btn",
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          autoplay: true,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          autoplay: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+          autoplay: true,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          autoplay: true,
+  
+        }
+      }
+    ]
+  });
+  $('.slider_2').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    // autoplay: true,
+    speed: 900,
+  
+  });
